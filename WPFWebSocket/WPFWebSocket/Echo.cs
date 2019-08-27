9dd3ed5e217d924e71d98f035e4915db6e41fca3
KimHeeRyeong <khr95039@gmail.com>
2019-08-27 11:17:38 +0900

    Feat  Add chatting service
    - webSocketServer
    - Echo Service

---
+++ WPFWebSocket/WPFWebSocket/Echo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+
+namespace WPFWebSocket
+    //Echo는 유저 1개당 1개씩 생성됨
+{
+    public class Echo : WebSocketBehavior//외부에서 메세지를 보낼때 반응
+    {
+        static List<Echo> listUser = new List<Echo>();
+        string nick = "";
+        protected override void OnOpen() {//유저가 접속했을때
+            listUser.Add(this);    
+        }
+        protected override void OnMessage(MessageEventArgs e) {
+            lock (listUser)
+            {
+                if (nick.Length == 0)
+                {
+                    nick = e.Data;
+                    this.Send("닉네임" + nick + "님이 접속했습니다.");
+
+                    int count = listUser.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (listUser[i] != this)
+                        {
+                            listUser[i].Send(nick + "님이 접속했습니다.");
+                        }
+                    }
+                }
+                else
+                {
+                    int count = listUser.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (listUser[i].nick.Length != 0)
+                        {
+                            listUser[i].Send(nick + ":" + e.Data);
+                        }
+                    }
+                }
+            }
+        }
+        protected override void OnClose(CloseEventArgs e){//유저가 나갔을때
+            lock (listUser)//블로킹
+            {
+                int count = listUser.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    if (listUser[i] != this)
+                    {
+                        listUser[i].Send(nick + "님이 나갔습니다.");
+                    }
+                }
+                listUser.Remove(this);
+            }
+        }
+        protected override void OnError(ErrorEventArgs e) {//에러. 보통 OnClose 호출해 접속을 끊어버림
+        }
+
+    }
+}
--- WPFWebSocket/WPFWebSocket/MainWindow.xaml.cs
+++ WPFWebSocket/WPFWebSocket/MainWindow.xaml.cs
@@ -12,17 +12,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
 
 namespace WPFWebSocket
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        const int PORT = 9999;
+
+        WebSocketServer webSocket;
+
         public MainWindow()
         {
             InitializeComponent();
+            webSocket = new WebSocketServer(PORT);
+            webSocket.AddWebSocketService<Echo>("/Echo");//이 주소로 접속하면 Echo실행
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!webSocket.IsListening)
+            {
+                webSocket.Start();
+                btStart.Content = "Stop Server";
+            }
+            else
+            {
+                webSocket.Stop();
+                btStart.Content = "Start Server";
+            }
         }
     }
 }
